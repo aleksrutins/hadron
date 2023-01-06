7f4401da555cb43e68e754b9b1b56d53d7aa1011
Aleks Rutins <aleks@rutins.com>
2023-01-06 16:49:47 -0500

    Use Blueprints

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ main (int   argc,
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
 
-	app = hadron_application_new ("com.rutins.Hadron", G_APPLICATION_FLAGS_NONE);
+	app = hadron_application_new ("com.rutins.Hadron", G_APPLICATION_DEFAULT_FLAGS);
 	ret = g_application_run (G_APPLICATION (app), argc, argv);
 
 	return ret;
